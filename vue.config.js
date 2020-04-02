4504acdbb30058e11fd055916c3ae9257d7ca5b3
Billy Charlton <billy@okbecause.com>
2020-04-02 14:58:07 +0200

    add markdown loader

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/covid-sim/' : '/',
   productionSourceMap: false,
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.md$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+            {
+              loader: 'markdown-loader',
+              options: {
+                /* your options here */
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
 }
