edb3357577fcfd4ed621e553b5ff0e53882d29da
Billy Charlton <charlton@vsp.tu-berlin.de>
2020-04-23 14:20:43 +0200

    ModalDialog from Bulma

---
+++ src/components/ModalDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="modal">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Modal title</p>
+        <button class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">{{ markdown }}</section>
+      <footer class="modal-card-foot">
+        <button class="button is-success">Save changes</button>
+        <button class="button">Cancel</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch, Prop } from 'vue-property-decorator'
+
+@Component({ components: {}, props: {} })
+export default class VueComponent extends Vue {
+  @Prop({ required: true }) private markdown!: string
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles.scss';
+
+@media only screen and (max-width: 640px) {
+}
+</style>
