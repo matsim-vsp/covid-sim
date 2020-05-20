f55092af304431070e125848616004c2c67d8b68
Billy Charlton <billy@okbecause.com>
2020-05-20 18:42:45 +0200

    hotfix: home link

--- src/components/TopNavBar.vue
+++ src/components/TopNavBar.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   nav.navbar.is-link(role="navigation" aria-label="main navigation")
     .navbar-brand
-      router-link.home-link.navbar-item(to="/") Home
+      router-link.home-link.navbar-item(to="/covid-sim") Home
         //- img(src="https://bulma.io/images/bulma-logo.png" width="112" height="28")
 
       a.navbar-burger.burger(
