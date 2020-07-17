1f6bb184e989b02ef8a9a72d9d7dd7340e992409
Billy Charlton <billy@okbecause.com>
2020-07-17 18:47:57 +0200

    debug

--- scripts/build-travis.sh
+++ scripts/build-travis.sh
@@ -9,15 +9,17 @@ set -euo pipefail
 IFS=$'\n\t'
 # -----------------------------------------------------
 
+echo BUILD: Getting RKI_FILE
+
 RKI_FILE=https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data
 
 wget -nv -O rki.csv $RKI_FILE
 
+echo BUILD: Create [city]-cases.csv files
 # create the [city]-cases.csv files
 python3 scripts/rki-update.py rki.csv
-# yarn run build && yarn run test:unit
-# exit 0
 
+echo BUILD: Check out SVN and move *.csv files
 # put them in the right places
 svn checkout --username $SVN_USER --password $SVN_PASSWORD --no-auth-cache --depth infinity \
     https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/episim/original-data/Fallzahlen/
@@ -25,27 +27,34 @@ svn checkout --username $SVN_USER --password $SVN_PASSWORD --no-auth-cache --dep
 cp *cases* src/assets
 cp *cases* Fallzahlen/RKI
 
+echo BUILD: Commit cases to SVN
+
 TIMESTAMP=`date`
 svn commit --username $SVN_USER --password $SVN_PASSWORD  --no-auth-cache -m "autobuild: $TIMESTAMP" Fallzahlen
 
 # Get Berlin hospital data too
 cp Fallzahlen/Berlin/berlin-hospital.csv src/assets
 
+echo BUILD: Process DIVI Cases
 # --- process DIVI cases for Berlin.
 # 1. fetch existing csv's from SVN:
 svn checkout --username $SVN_USER --password $SVN_PASSWORD --no-auth-cache --depth infinity \
     https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/episim/original-data/Fallzahlen/DIVI/
 
+echo BUILD: Scrape CSVs from DIVI site
 # 2. scrape most recent csv from website:
 cd DIVI/Daily_reports
 export SRC="https://www.divi.de"
 export FOLDER="/divi-intensivregister-tagesreport-archiv-csv"
 
-#wget -qO - ${SRC}${FOLDER} \
-#  | grep "\"DIVI\-" \
-#  | head -1 \
-#  | awk -v src=$SRC -F "\"" '// {print $4 ".csv", src $2}' \
-#  | xargs wget -qO
+wget -qO - ${SRC}${FOLDER} > datafile
+grep "\"DIVI\-" < datafile > datafile2
+
+head -1 datafile2 \
+  | awk -v src=$SRC -F "\"" '// {print $4 ".csv", src $2}' \
+  | xargs wget -qO
+
+echo BUILD: Grep files into processed.csvs
 
 BERLIN_CODE="11000"
 printf "csv,gemeindeschluessel,anzahl_meldebereiche,faelle_covid_aktuell,faelle_covid_aktuell_beatmet,anzahl_standorte,betten_frei,betten_belegt,daten_stand\n" > ../berlin-divi-processed.csv
@@ -54,6 +63,8 @@ grep $BERLIN_CODE *.csv  |  sort  >>  ../berlin-divi-processed.csv
 MUNICH_CODE="9162"
 printf "csv,gemeindeschluessel,anzahl_meldebereiche,faelle_covid_aktuell,faelle_covid_aktuell_beatmet,anzahl_standorte,betten_frei,betten_belegt,daten_stand\n" > ../munich-divi-processed.csv
 grep $MUNICH_CODE *.csv  |  sort  >>  ../munich-divi-processed.csv
+
+echo BUILD: svn add and commit
 
 # svn st | sed -rn '/^\?/s/^.{8}(.+)$/\1/p' | xargs -r svn add
 
@@ -65,5 +76,7 @@ cd ../..
 # svn commit --username $SVN_USER --password $SVN_PASSWORD  --no-auth-cache -m "DIVI autobuild: $TIMESTAMP" DIVI
 
 # DONE WITH PREP! Build the site.
+echo BUILD: Finally let's build the site
+
 yarn run build && yarn run test:unit
 
