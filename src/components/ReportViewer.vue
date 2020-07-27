015f42313378a3bafa9ad47882d24b8ccebb73b9
AartSteinmann <66619351+AartSteinmann@users.noreply.github.com>
2020-07-27 12:11:54 +0200

    Update ReportViewer.vue

--- src/components/ReportViewer.vue
+++ src/components/ReportViewer.vue
@@ -24,6 +24,12 @@ export default class VueComponent extends Vue {
   private mdParser = new MarkdownIt()
 
   private reports: any[] = [
+  {
+      md: '/reports/report-2020-07-03.md',
+      title: '03 July 2020',
+      url: 'http://dx.doi.org/10.14279/depositonce-10417',
+      html: '',
+    },
     {
       md: '/reports/report-2020-07-03.md',
       title: '03 July 2020',
