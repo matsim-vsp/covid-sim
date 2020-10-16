a2595f544ea1b5d84a46a71d5f6d7fbc56957379
Billy Charlton <billy@okbecause.com>
2020-10-16 13:57:16 +0200

    remove lots of console.log()

--- src/components/RValuePlot.vue
+++ src/components/RValuePlot.vue
@@ -46,8 +46,6 @@ export default class VueComponent extends Vue {
 
     this.$emit('method', 'Based on tracking all infections')
 
-    console.log({ RVALUES: this.rValues })
-
     const x: any[] = []
     const y: any[] = []
 
