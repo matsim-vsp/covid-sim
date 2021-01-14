31d6784566b15a9ce037b6299b11238c4733481b
Billy Charlton <billy@okbecause.com>
2021-01-14 16:45:50 +0100

    ALM e.V.

--- src/views/battery-viewer/SingleRunViewer.vue
+++ src/views/battery-viewer/SingleRunViewer.vue
@@ -1138,7 +1138,7 @@ export default class VueComponent extends Vue {
         marker: { color: '#f42', size: 4 },
       },
       {
-        name: 'RKI: Share Positive Tests',
+        name: 'ALM e.V.: Share Positive Tests',
         x: sDates,
         y: sShare,
         text: text,
