6ce5e523348cbe62f62ceafbe4144c287727a00d
frie_voe <friedrich.k.voelkers@tu-berlin.de>
2021-05-03 00:29:16 +0200

    Changed x-axis max date

--- src/components/MobilityPlot.vue
+++ src/components/MobilityPlot.vue
@@ -206,7 +206,7 @@ export default class VueComponent extends Vue {
     margin: { t: 5, r: 60, b: 0, l: 60 },
     xaxis: {
       fixedrange: window.innerWidth < 700,
-      range: ['2020-03-02', '2021-04-21'],
+      range: ['2020-03-02', '2021-04-26'],
       type: 'date',
     },
     yaxis: {
