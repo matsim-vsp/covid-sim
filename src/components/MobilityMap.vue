16b7c9374f97d54d969b3b79672bfb02b8d0b8c6
frie_voe <friedrich.k.voelkers@tu-berlin.de>
2021-06-09 16:50:59 +0200

    added geojson to layout

--- src/components/MobilityMap.vue
+++ src/components/MobilityMap.vue
@@ -23,6 +23,14 @@ export default class VueComponent extends Vue {
     var names = []
     var geometries = []
 
+    var data = {
+      type: 'feature',
+      geometry: {
+        type: 'MultiPolygon',
+        coordinates: [],
+      },
+    }
+
     const response = await fetch(
       'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/episim/original-data/landkreise-in-germany/landkreise-in-germany.geojson'
     )
@@ -31,9 +39,13 @@ export default class VueComponent extends Vue {
     for (var i = 0; i < jsonData.features.length; i++) {
       geometries.push(jsonData.features[i].geometry.coordinates[0][0])
       names.push(jsonData.features[i].properties.name_2)
+      data.geometry.coordinates = jsonData.features[i].geometry.coordinates
+      this.layout.mapbox.layers[0].source.features.push(data)
     }
 
     //this.layout.mapbox.layers[0].source.features.push(jsonData.features[i])
+
+    console.log(this.layout.mapbox.layers[0].source.features)
 
     return jsonData
   }
@@ -49,10 +61,10 @@ export default class VueComponent extends Vue {
 
   private layout = {
     mapbox: {
-      //center: { lon: 10.3, lat: 51.3 },
-      //zoom: 5.3,
-      center: { lon: -73.6, lat: 45.505 },
-      zoom: 13,
+      center: { lon: 10.3, lat: 51.3 },
+      zoom: 5.3,
+      //center: { lon: -73.6, lat: 45.505 },
+      //zoom: 13,
       style: 'basic',
       layers: [
         {
@@ -100,6 +112,52 @@ export default class VueComponent extends Vue {
                         [-73.620122362, 45.524377961],
                         [-73.620201713, 45.524298907],
                         [-73.620775593, 45.523650879],
+                      ],
+                    ],
+                  ],
+                },
+              },
+              {
+                type: 'Feature',
+                geometry: {
+                  type: 'MultiPolygon',
+                  coordinates: [
+                    [
+                      [
+                        [-73.606352888, 45.607489991],
+                        [-73.606133883, 45.606876],
+                        [-73.605905904, 45.60677398],
+                        [-73.603533905, 45.605698946],
+                        [-73.60247587, 45.606856969],
+                        [-73.600031904, 45.605696003],
+                        [-73.599379992, 45.605389066],
+                        [-73.599119902, 45.605632008],
+                        [-73.598896977, 45.605514039],
+                        [-73.598783894, 45.605617001],
+                        [-73.591308727, 45.616246185],
+                        [-73.591380782, 45.616280145],
+                        [-73.596778656, 45.618690062],
+                        [-73.60279677, 45.621348046],
+                        [-73.612239983, 45.625564037],
+                        [-73.612422919, 45.625642061],
+                        [-73.617229085, 45.627751983],
+                        [-73.617279234, 45.62777416],
+                        [-73.617304713, 45.627741334],
+                        [-73.617492052, 45.627498362],
+                        [-73.617533258, 45.627512253],
+                        [-73.618074188, 45.626759105],
+                        [-73.618271651, 45.626500673],
+                        [-73.61844632, 45.626287943],
+                        [-73.618968507, 45.62569856],
+                        [-73.619388002, 45.62521675],
+                        [-73.619532966, 45.625064183],
+                        [-73.619686662, 45.62488929],
+                        [-73.619787038, 45.624770086],
+                        [-73.619925742, 45.624584939],
+                        [-73.619954486, 45.62455769],
+                        [-73.620122362, 45.624377961],
+                        [-73.620201713, 45.624298907],
+                        [-73.620775593, 45.623650879],
                       ],
                     ],
                   ],
