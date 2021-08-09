5f1da28d6d73c9a55a97a0b5cdf72e4391cad6f8
frie_voe <friedrich.k.voelkers@tu-berlin.de>
2021-08-09 16:49:11 +0200

    Small bug fixes

--- src/views/MobilityPageLandkreise.vue
+++ src/views/MobilityPageLandkreise.vue
@@ -314,6 +314,8 @@ export default class VueComponent extends Vue {
       this.landkreise + this.timeline + this.weekdaysTimeline
     )
 
+    console.log(this.timelineyWeekdays)
+
     this.timelineWeekly = await this.loadLandkreiseTimeline(
       this.landkreise + this.timeline + this.weekly
     )
@@ -332,6 +334,8 @@ export default class VueComponent extends Vue {
     )
 
     this.combineData()
+
+    console.log(this.allData)
 
     this.updateLandkreisNames()
 
@@ -461,9 +465,10 @@ export default class VueComponent extends Vue {
         this.allWeekdayDates.push(date)
       }
       var duration = this.mobilityWeekdays[i].outOfHomeDuration
+      //console.log(duration)
       if (this.allData[landkreis] !== undefined) {
         this.allData[landkreis]['weekday'][date] = {
-          outOfHomeDuration: this.mobilityWeekends[i].outOfHomeDuration,
+          outOfHomeDuration: duration,
           percentageChangeComparedToBeforeCorona: this.mobilityWeekdays[i]
             .percentageChangeComparedToBeforeCorona,
           sharePersonLeavingHome: sharePerson,
@@ -477,7 +482,7 @@ export default class VueComponent extends Vue {
       var landkreis = this.mobilityWeekly[i].Landkreis
       var date = this.rangeWeekly[i].date
       var dailyRange = this.rangeWeekly[i].dailyRangePerPerson
-      var sharePerson = this.rangeWeeends[i].sharePersonLeavingHome
+      var sharePerson = this.rangeWeekly[i].sharePersonLeavingHome
       if (landkreis == 'Landkreis St, Wendel') {
         landkreis = 'Landkreis St. Wendel'
       } else if (landkreis == 'Cottbus - Chó?ebuz') {
@@ -528,6 +533,7 @@ export default class VueComponent extends Vue {
           this.minWeekMobility = sum
         }
         this.allData[area]['weekday'][date].endHomeActs = sum
+        console.log(sum)
       }
     }
 
