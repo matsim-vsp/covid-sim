a532f560061870a3f5cbfadc095e2fbaafe98e51
frievoe97 <frievoe97@users.noreply.github.com>
2024-12-18 10:21:45 +0000

    deploy: 621e1ef4f59bfef4e9fa67e9fe7fb931786c12a7

--- assets/MessagePage-BJ99qHfX.js
+++
@@ -1,9 +0,0 @@
-import{M as t,d as s,n as r}from"./index-B2iCMRv8.js";const a=`### No results for that model
-
-Not all model versions have published results.
-
-**Version 3** (v3) only has the animated visualization published, which can be seen [here](/v3).
-
-**Version 6** (v6) was an internal draft, and its outputs were quickly superceded by [version 7 here](/v7).
-`,o=new t({html:!0,linkify:!0,typographer:!0}),i=s({name:"MessagePane",data(){return{mdContent:"",currentCity:-1}},mounted(){this.mdContent=o.render(a)}});var d=function(){var e=this,n=e._self._c;return e._self._setupProxy,n("div",{staticClass:"bavue-component"},[n("div",{staticClass:"content"},[n("div",{staticClass:"stuff",domProps:{innerHTML:e._s(e.mdContent)}}),n("hr"),n("p",[e._v("Go back to the "),n("router-link",{attrs:{to:"/"}},[e._v("main page.")])],1)])])},l=[],u=r(i,d,l,!1,null,"b5dd9462");const v=u.exports;export{v as default};
-//# sourceMappingURL=MessagePage-BJ99qHfX.js.map
