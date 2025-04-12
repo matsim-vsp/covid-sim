41fc856d57138bacfa095210749026e21f100f09
frievoe97 <frievoe97@users.noreply.github.com>
2025-04-12 12:11:33 +0000

    deploy: fc3d36242dc3414c395e63c7ed8b00b89a44507d

--- docs2/assets/404.html-Dj_2ePY_.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,a,o as n}from"./app-BNOHG9Bg.js";const l={};function r(s,t){return n(),o("div",null,t[0]||(t[0]=[a("p",null,"404 Not Found",-1)]))}const d=e(l,[["render",r]]),_=JSON.parse('{"path":"/404.html","title":"","lang":"de-DE","frontmatter":{"layout":"NotFound"},"headers":[],"git":{},"filePathRelative":null}');export{d as comp,_ as data};
